57485165919fddd5637789f62cb87546a85d7fb5
iamanatolich <alex-goncharov@inbox.ru>
2025-01-10 22:50:44 +0300

    Обновил данные почты

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ from fastapi_mail import ConnectionConfig
 conf = ConnectionConfig(
     MAIL_PORT=587,
     MAIL_SERVER="smtp.yandex.ru",
-    MAIL_USERNAME="Rebuild-Pro@yandex.ru",
+    MAIL_USERNAME="support@rebuildpro.ru",
     MAIL_STARTTLS=True,
     MAIL_SSL_TLS=False,
-    MAIL_FROM="Rebuild-Pro@yandex.ru",
-    MAIL_PASSWORD="fkevtzlpolrsdhip",
+    MAIL_FROM="support@rebuildpro.ru",
+    MAIL_PASSWORD="gnpabcowlbnystjz",
     USE_CREDENTIALS=True,
     VALIDATE_CERTS=True
 )
